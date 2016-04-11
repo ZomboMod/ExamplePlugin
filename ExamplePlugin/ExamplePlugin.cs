5dd708d4ab1053b585644dd336aee5dc0b0e7c8a
Leonardo Santos <devleeo@outlook.com>
2016-04-11 07:02:25 -0300

    Initnal commit

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ZomboMod;
+using ZomboMod.Plugin;
+
+namespace ExamplePlugin
+{
+    [PluginInfo("ExamplePlugin", "leonardosnt", "1.0.0")]
+    public class ExamplePlugin : PluginBase
+    {
+        public override void OnLoad()
+        {
+            Console.WriteLine( "Working :D" );
+
+            Zombo.Server.Name = "That is working!!";
+        }
+    }
+}
